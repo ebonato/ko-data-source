922d82f530f90dbbf22bca30e3e16d1d2aae5c52
Eduardo Bonato <ebonato@gmail.com>
2017-04-04 14:48:00 -0300

    Add features to DefaultObservableStateTransitioner
    ADD: Make DefaultObservableStateTransitioner compatible with complex
    objects
    ADD: If object already has an observable (or computed) it will use it
    instead of creating new observables.
    ADD: If you need to rebind grid with more columns (dynamic columns),
    than you can update properties managed by
    DefaultObservableStateTransitioner

--- src/default-observable-state-transitioner.js
+++ src/default-observable-state-transitioner.js
@@ -17,14 +17,27 @@ define(['knockout'], function (ko) {
                 return entry;
 
             var observable = {};
-
-            Object.keys(entry).forEach(p => {
-                if (isObservableProperty && isObservableProperty[p])
-                    observable[p] = ko.observable(entry[p]);
-                else
-                    observable[p] = entry[p];
-            });
-
+            var propertyNameParent = '';
+            var makeObservable = function (o, bindingObservable) //create observables recursively
+            {
+               Object.keys(o).forEach(function (p) {
+                  if (typeof o[p] === 'object' && o[p] != null) {
+                     bindingObservable[p] = o[p];
+                     if (o[p]) {
+                        propertyNameParent += p + '.';
+                        makeObservable(o[p], bindingObservable[p]);
+                        propertyNameParent = propertyNameParent.replace(p + '.', '');
+                     }
+                  }
+                  else {
+                     if (isObservableProperty && isObservableProperty[propertyNameParent + p])
+                        bindingObservable[p] = ko.isObservable(o[p]) ? o[p] : ko.observable(o[p]);  //if property is already an observable, then uses it
+                     else
+                        bindingObservable[p] = o[p];
+                  }
+               });
+            }
+            makeObservable(entry, observable);
             return observable;
         },
         'updater': function (observable, updatedEntry) {
@@ -32,13 +45,35 @@ define(['knockout'], function (ko) {
             if (!isObservableProperty)
                 return observable;
 
-            Object.keys(updatedEntry)
-                .filter(p => isObservableProperty && isObservableProperty[p])
-                .forEach(p => observable[p](updatedEntry[p]));
-
+            var updateObservable = function (o, bindingObservable) //update observables recursively
+            {
+               Object.keys(o).forEach(function (p) {
+                  if (typeof o[p] === 'object' && o[p] != null) {
+                     if (o[p] && bindingObservable) {
+                        updateObservable(o[p], bindingObservable[p]);
+                     }
+                  }
+                  else {
+                     if (ko.isObservable(bindingObservable[p])) {
+                        if (ko.isWritableObservable(bindingObservable[p]))
+                           bindingObservable[p](ko.unwrap(o[p]));
+                     }
+                     else
+                        bindingObservable[p] = ko.unwrap(o[p]);
+                  }
+               });
+            }
+            updateObservable(updatedEntry, observable);
             return observable;
         },
-        'destructor': function () {}
+        'destructor': function () {},
+        'updateProperties': function(options) {
+          this.__isObservableProperty = false;
+          (options && options['observableProperties'] || []).forEach(function(p) {
+              this.__isObservableProperty = this.__isObservableProperty || {};
+              this.__isObservableProperty[p] = true;
+          }.bind(this));
+      }
     };
 
     return DefaultObservableStateTransitioner;
